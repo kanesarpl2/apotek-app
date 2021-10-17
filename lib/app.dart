1916cf8befaade886db296f06ed32111ef14744a
Agus Prayogi <agus21apy@gmail.com>
2021-10-17 14:51:10 +0700

    (+) adding file assets

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,14 @@
 import 'package:apotek_bloc/presentation/admin/home/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-class App extends MaterialApp {
-  const App({Key? key}) : super(key: key, home: const HomePage());
+class App extends GetMaterialApp {
+  const App({Key? key})
+      : super(
+          key: key,
+          home: const HomePage(),
+          routes: const {},
+          title: "Apotek App",
+          debugShowCheckedModeBanner: false,
+        );
 }
